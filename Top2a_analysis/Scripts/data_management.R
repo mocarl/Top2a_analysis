c975e1ad229fdbd48988534c9f528e9426568589
mocarl <50395597+mocarl@users.noreply.github.com>
2022-05-10 10:57:24 +0200

    data management functions
    funcitons updated to work with single folders. Will not work if top folder contians ext

--- Top2a_analysis/Scripts/data_management.R
+++ Top2a_analysis/Scripts/data_management.R
@@ -3,11 +3,11 @@
 ## Author: Carl Möller mocarl@chalmers.se
 
 #Move mask imagease from Imagej to a separate folder for easier overview
-move_outlines("/Volumes/Carl_Ext_2/Evgeniya_TOP2a/2nd_iteration/with_ATP/2022-04-11-rel pFLIP-FUSE + 10 nM TOP2A")
+move_outlines("/Volumes/Carl_Ext_2/EXPERIMENTS/20220423/Top2__FLIP_sc_10nM_wATP")
 
 #Consolidate all csv files from each channel into one xlsx file with a separate sheet for every image
-data_consolidation(path= "/Volumes/Carl_Ext_2/Evgeniya_TOP2a/2nd_iteration/with_ATP", C0 =  "Top2a"  , C1 = "YOYO1" )
+data_consolidation(path= "/Volumes/Carl_Ext_2/EXPERIMENTS/20220423/Top2__FLIP_sc_10nM_wATP", C0 =  "Top2a"  , C1 = "YOYO1" )
 
 # Copies data from a source to the Data folder contained in the R project environment.
 # This is necessary for functions to find the data. Make note that the data should not be pushed to repository
-copy_data(from = "/Volumes/Carl_Ext_2/Evgeniya_TOP2a/2nd_iteration/with_ATP", ext = ".xlsx")
+copy_data(from = "/Volumes/Carl_Ext_2/EXPERIMENTS/20220423/Top2__FLIP_sc_10nM_wATP", ext = ".xlsx")
