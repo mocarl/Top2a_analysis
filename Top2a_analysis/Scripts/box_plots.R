da4c8b2e8324492f6a828c2a120607a610ce9b38
mocarl <50395597+mocarl@users.noreply.github.com>
2022-06-09 15:40:48 +0200

    box plot script added
    Script to generate box plots

---
+++ Top2a_analysis/Scripts/box_plots.R
@@ -0,0 +1,91 @@
+### Statistical analysis of Top2a deposited on funcitonalised glass slides
+## Generates graphs from colocalisation statistics
+## Author: Carl Möller mocarl@chalmers.se
+
+### Import and arrange data into one dataframe
+import_csv("Output/pFLIP/pFLIP_relaxed_10nM_Top2a_noATP")
+import_csv("Output/pFLIP/pFLIP_supercoiled_10nM_Top2a_noATP")
+import_csv("Output/pFLIP_FUSE/pFLIP_FUSE_relaxed_10nM_Top2a_noATP")
+import_csv("Output/pFLIP_FUSE/pFLIP_FUSE_supercoiled_10nM_Top2a_noATP")
+rm(pFLIP_relaxed_10nM_Top2a_noATP__coloc_statistics)
+rm(pFLIP_supercoiled_10nM_Top2a_noATP__coloc_statistics)
+rm(pFLIP_FUSE_relaxed_10nM_Top2a_noATP__coloc_statistics)
+rm(pFLIP_FUSE_supercoiled_10nM_Top2a_noATP__coloc_statistics)
+var = setdiff(ls(), lsf.str())
+
+plasmid=c("pFLIP-supercoiled",
+       "pFLIP-supercoiled",
+       "pFLIP-relaxed",
+       "pFLIP-relaxed",
+       "pFLIP-FUSE-supercoiled",
+       "pFLIP-FUSE-supercoiled",
+       "pFLIP-FUSE-relaxed",
+       "pFLIP-FUSE-relaxed")
+temp.data = data.frame()
+for (i in 1:length(var)){
+    temp.data = rbind(temp.data, data.frame(get(paste0(var[i])),Experiment = paste0(var[i]), Plasmid=paste(plasmid[i])))
+    }
+temp.data$Experiment = factor(temp.data$Experiment, levels = temp.data$Experiment)
+## Generate ridgeplot with density and histogram overlay
+#Unicode alpha = \u03b1
+expression(paste0("pFLIP-supercoiled \nTop2a"))
+#Define labels for y-axis ticks
+ylab=c("pFLIP-supercoiled \nYOYO-1",
+       "pFLIP-supercoiled \nTop2\u03b1",
+       "pFLIP-relaxed \nYOYO-1",
+       "pFLIP-relaxed \nTop2\u03b1",
+       "pFLIP-FUSE-supercoiled \nYOYO-1",
+       "pFLIP-FUSE-supercoiled \nTop2\u03b1",
+       "pFLIP-FUSE-relaxed \nYOYO-1",
+       "pFLIP-FUSE-relaxed \nTop2\u03b1")
+
+temp.data <- temp.data %>%
+  group_by(Experiment) %>% 
+  mutate(value_norm = IntDen - min(IntDen), 
+         value_norm = value_norm / max(value_norm))
+
+### Area distribution with median
+tiff(file=paste("/Users/mocarl/Library/CloudStorage/OneDrive-ChalmersUniversityofTechnology/Top2a_project/Figures/Figure 2/","box_plot_area",".tiff"), width = 10, height = 10, units = "in", res = 300, pointsize = 7)
+ggplot(temp.data, aes(y = Area, x = Experiment, fill = Coloc)) +
+  geom_boxplot()+
+  facet_wrap(~Plasmid, scale="free")+
+  labs(title = 'Particle area', subtitle = "10nM Top2\u03b1 - 250nM pFLIP/pFLIP-FUSE - w/o ATP") +
+  theme(
+    legend.position="right",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8),
+    axis.text.y = element_text(vjust = 0, face="bold", size = 10))+
+  theme_minimal()+
+  xlab("Particle area distribution") +
+  ylab("")+
+  ylim(0,5)+
+  scale_x_discrete(breaks=var,labels=ylab)+
+  scale_fill_manual(
+    name = "Population", values = c("#E1BE6A", "#40B0A6"),
+    labels = c(paste0("Non-colocalised"),paste0("Colocalised")))
+dev.off()
+
+### Sum of pixel intensity per particle with median
+tiff(file=paste("/Users/mocarl/Library/CloudStorage/OneDrive-ChalmersUniversityofTechnology/Top2a_project/Figures/Figure 2/","box_plot_IntDen",".tiff"), width = 10, height = 10, units = "in", res = 300, pointsize = 7)
+ggplot(temp, aes(y =value_norm, x = Experiment, fill = Coloc)) +
+  geom_boxplot()+
+  facet_wrap(~Plasmid, scale="free")+
+  labs(title = 'Particle integrated intensity', subtitle = "10nM Top2\u03b1 - 250nM pFLIP/pFLIP-FUSE - w/o ATP") +
+  theme(
+    legend.position="right",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8),
+    axis.text.y = element_text(vjust = 0, face="bold", size = 10))+
+  theme_minimal()+
+  xlab("Particle integrated intensity") +
+  ylab("")+
+  ylim(0,0.1)+
+  scale_x_discrete(breaks=var,labels=ylab)+
+  scale_fill_manual(
+    name = "Population", values = c("#E1BE6A", "#40B0A6"),
+    labels = c(paste0("Non-colocalised"),paste0("Colocalised")))
+dev.off()
+
+
+
+
--- Top2a_analysis/Scripts/ridge_plots.R
+++ Top2a_analysis/Scripts/ridge_plots.R
@@ -4,11 +4,13 @@
 
 ### Import and arrange data into one dataframe
 import_csv("Output/pFLIP/pFLIP_relaxed_10nM_Top2a_noATP")
-import_csv("Output/pFLIP/pFLIP_relaxed_10nM_Top2a_ATP")
-import_csv("Output/pFLIP/pFLIP_relaxed_10nM_Top2a_yATP")
+import_csv("Output/pFLIP/pFLIP_supercoiled_10nM_Top2a_noATP")
+import_csv("Output/pFLIP_FUSE/pFLIP_FUSE_relaxed_10nM_Top2a_noATP")
+import_csv("Output/pFLIP_FUSE/pFLIP_FUSE_supercoiled_10nM_Top2a_noATP")
 rm(pFLIP_relaxed_10nM_Top2a_noATP__coloc_statistics)
-rm(pFLIP_relaxed_10nM_Top2a_ATP__coloc_statistics)
-rm(pFLIP_relaxed_10nM_Top2a_yATP__coloc_statistics)
+rm(pFLIP_supercoiled_10nM_Top2a_noATP__coloc_statistics)
+rm(pFLIP_FUSE_relaxed_10nM_Top2a_noATP__coloc_statistics)
+rm(pFLIP_FUSE_supercoiled_10nM_Top2a_noATP__coloc_statistics)
 var = setdiff(ls(), lsf.str())
 temp.data = data.frame()
 for (i in var){
@@ -17,13 +19,23 @@ for (i in var){
 temp.data$Experiment = factor(temp.data$Experiment, levels = temp.data$Experiment)
 ## Generate ridgeplot with density and histogram overlay
 
+#Define labels for y-axis ticks
+ylab=c("pFLIP-supercoiled \nYOYO1",
+       "pFLIP-supercoiled \nTop2a",
+       "pFLIP-relaxed \nYOYO1",
+       "pFLIP-relaxed \nTop2a",
+       "pFLIP-FUSE-supercoiled \nYOYO1",
+       "pFLIP-FUSE-supercoiled \nTop2a",
+       "pFLIP-FUSE-relaxed \nYOYO1",
+       "pFLIP-FUSE-relaxed \nTop2a")
+
 ### Area distribution with median
 tiff(file=paste("Output/","Graphs/","pFLIP/","pFLIP_relaxed_10nM_TOP2_area_dist_yATP",".tiff"), width = 10, height = 10, units = "in", res = 300, pointsize = 7)
 ggplot(temp.data, aes(x = Area, y = Experiment, fill = Coloc, height=..density..,scale=1)) +
-  geom_density_ridges(stat = "binline", bins=250,
+  geom_density_ridges(stat = "binline", bins=150,
                       draw_baseline = F, alpha = 1,lwd=0.1 )+
   geom_density_ridges(quantile_lines = TRUE, quantiles = 2, rel_min_height = 0.001, alpha = 0.4, lwd=0.3, colour="black")+
-  labs(title = 'Particle area distribution', subtitle = "10nM pFLIP relaxed w/wo ATP") +
+  labs(title = 'Particle area distribution', subtitle = "10nM pFLIP/pFLIP-FUSE w/o ATP") +
   theme(
     legend.position="right",
     panel.spacing = unit(0.1, "lines"),
@@ -33,7 +45,7 @@ ggplot(temp.data, aes(x = Area, y = Experiment, fill = Coloc, height=..density..
   xlab("Particle area histogram/density with median") +
   ylab("Condition")+
   xlim(0,5)+
-  scale_y_discrete(breaks=var,labels=c("pFLIP-relaxed \nTop2a - yATP","pFLIP-relaxed \nYOYO1 - yATP","pFLIP-relaxed \nTop2a- ATP","pFLIP-relaxed \nYOYO1 - ATP","pFLIP-relaxed \nTop2a - no ATP","pFLIP-relaxed \nYOYO1 - no ATP"))+
+  scale_y_discrete(breaks=var,labels=ylab)+
   scale_fill_manual(
     name = "Population", values = c("#E1BE6A", "#40B0A6"),
     labels = c(paste0("Non-colocalised"),paste0("Colocalised")))
@@ -41,11 +53,11 @@ dev.off()
 
 ### Sum of pixel intensity per particle with median
 tiff(file=paste("Output/","Graphs/","pFLIP/","pFLIP_relaxed_10nM_TOP2_intensity_dist_yATP",".tiff"), width = 10, height = 10, units = "in", res = 300, pointsize = 7)
-ggplot(temp.data, aes(x = RawIntDen, y = Experiment, fill = Coloc, height=..density..,scale=1)) +
-  geom_density_ridges(stat = "binline", bins=250,
+ggplot(temp.data, aes(x = IntDen, y = Experiment, fill = Coloc, height=..density..,scale=1)) +
+  geom_density_ridges(stat = "binline", bins=150,
                       draw_baseline = F, alpha = 1,lwd=0.1 )+
   geom_density_ridges(quantile_lines = TRUE, quantiles = 2, rel_min_height = 0.001, alpha = 0.4, lwd=0.3, colour="black")+
-  labs(title = 'Summed pixel intensity per particle distribution', subtitle = "10nM pFLIP relaxed w/wo ATP") +
+  labs(title = 'Summed pixel intensity per particle', subtitle = "10nM pFLIP relaxed w/wo ATP") +
   theme(
     legend.position="right",
     panel.spacing = unit(0.1, "lines"),
@@ -54,8 +66,8 @@ ggplot(temp.data, aes(x = RawIntDen, y = Experiment, fill = Coloc, height=..dens
   theme_minimal()+
   xlab("Sum of pixel intensity per particle with median") +
   ylab("Condition")+
-  xlim(-1,1000)+
-  scale_y_discrete(breaks=var,labels=c("pFLIP-relaxed \nTop2a - yATP","pFLIP-relaxed \nYOYO1 - yATP","pFLIP-relaxed \nTop2a- ATP","pFLIP-relaxed \nYOYO1 - ATP","pFLIP-relaxed \nTop2a - no ATP","pFLIP-relaxed \nYOYO1 - no ATP"))+
+  xlim(-1,200)+
+  scale_y_discrete(breaks=var,labels=ylab)+
   scale_fill_manual(
     name = "Population", values = c("#E1BE6A", "#40B0A6"),
     labels = c(paste0("Non-colocalised"),paste0("Colocalised")))
