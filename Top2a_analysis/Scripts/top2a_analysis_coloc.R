1fe231bca8096695191add73e39c13df72b310bb
mocarl <calle.moller@gmail.com>
2022-08-31 17:57:52 +0200

    Update top2a_analysis_coloc.R

--- Top2a_analysis/Scripts/top2a_analysis_coloc.R
+++ Top2a_analysis/Scripts/top2a_analysis_coloc.R
@@ -10,7 +10,7 @@ source("Scripts/dependencies.R")
 
 ## Import the conditions you want to analyse by defining search words for the files names
 ## All subfolders in Data will be checked for matching files
-import_xlsx("Data/MYC/MYC",c("7114","results"))
+import_xlsx("Data/V3/1st",c("results"))
 
 ## Define the two channels you want to analyse. Channel1 is assumed to be channel of interest.
 ## Coloc is defined as particles having any overlapping point on a circle defined by the particles mass centre and a radius derived from particle area.
@@ -19,9 +19,8 @@ import_xlsx("Data/MYC/MYC",c("7114","results"))
 
 mass_coloc()
 
-coloc_particles("Experiment-7112.czi_split_Top2a_results","Experiment-7112.czi_split_YOYO1_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_75nM-MYC", c("YOYO1","Top2a"))
-coloc_particles("Experiment-7112.czi_split_Top2a_results","Experiment-7112.czi_split_MYC_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_75nM-MYC", c("MYC","Top2a"))
-coloc_particles("Experiment-7112.czi_split_MYC_results","Experiment-7112.czi_split_YOYO1_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_75nM-MYC", c("YOYO1","MYC"))
+coloc_particles("2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3_Top2a_results","2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3_YOYO1_results","Data/V3/1st/2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3")
+
 
 coloc_particles("Experiment-7114.czi_split_Top2a_results","Experiment-7114.czi_split_YOYO1_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_100nM-MYC", c("YOYO1","Top2a"))
 coloc_particles("Experiment-7114.czi_split_Top2a_results","Experiment-7114.czi_split_MYC_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_100nM-MYC", c("MYC","Top2a"))
