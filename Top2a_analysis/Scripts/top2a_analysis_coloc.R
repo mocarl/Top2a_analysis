f36772c7f5733d0a4ee7756f3ed28e2862968c8a
mocarl <50395597+mocarl@users.noreply.github.com>
2022-09-02 12:47:05 +0200

    Update top2a_analysis_coloc.R

--- Top2a_analysis/Scripts/top2a_analysis_coloc.R
+++ Top2a_analysis/Scripts/top2a_analysis_coloc.R
@@ -10,7 +10,8 @@ source("Scripts/dependencies.R")
 
 ## Import the conditions you want to analyse by defining search words for the files names
 ## All subfolders in Data will be checked for matching files
-import_xlsx("Data/V3/1st",c("results"))
+import_xlsx("Data/V3/MYC",c("results"))
+
 
 ## Define the two channels you want to analyse. Channel1 is assumed to be channel of interest.
 ## Coloc is defined as particles having any overlapping point on a circle defined by the particles mass centre and a radius derived from particle area.
@@ -19,11 +20,28 @@ import_xlsx("Data/V3/1st",c("results"))
 
 mass_coloc()
 
-coloc_particles("2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3_Top2a_results","2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3_YOYO1_results","Data/V3/1st/2022-05-04-rel_pFLIP-FUSE_+_10_nM_TOP2A_no_ATP_V3")
+#coloc_particles("2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_Top2a_results","2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+#coloc_particles("2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_Top2a_results","2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_MYC_results","Data/V3/MYC/2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+#coloc_particles("2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_MYC_results","2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP_FUSE_+_25_nM_TOP2A_+_100_nM_MYC", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
 
+#coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_Top2a_results","2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+#coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_Top2a_results","2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_MYC_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+#coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_MYC_results","2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_100_nM_MYC", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
 
-coloc_particles("Experiment-7114.czi_split_Top2a_results","Experiment-7114.czi_split_YOYO1_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_100nM-MYC", c("YOYO1","Top2a"))
-coloc_particles("Experiment-7114.czi_split_Top2a_results","Experiment-7114.czi_split_MYC_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_100nM-MYC", c("MYC","Top2a"))
-coloc_particles("Experiment-7114.czi_split_MYC_results","Experiment-7114.czi_split_YOYO1_results","Data/MYC/MYC/2022-07-06_scpFLIP-FUSE_100nM-MYC", c("YOYO1","MYC"))
+coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_Top2a_results","2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_Top2a_results","2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_MYC_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+coloc_particles("2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_MYC_results","2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A_V3_YOYO1_results","Data/V3/MYC/2022-06-14-sc_pFLIP-FUSE_+_25_nM_TOP2A", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
+
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_MYC_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_MYC_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_25nM-MYC", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
+
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_MYC_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_MYC_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_50nM-MYC", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
+
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC", label = c("YOYO1", "Top2a")) #Channel 1 = Top2a Channel 2 = YOYO1
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_Top2a_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_MYC_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC", label = c("MYC", "Top2a")) #Channel 1 = Top2a Channel 2 = MYC
+coloc_particles("2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_MYC_results","2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC_V3_YOYO1_results","Data/V3/MYC/2022-06-28_sc-pFLIP-FUSE_25nM-TOP2A_75nM-MYC", label = c("YOYO1", "MYC")) #Channel 1 = MYC Channel 2 = YOYO1
 
 import_csv("Data/2022-05-04-sc pFLIP-FUSE + 10 nM TOP2A no ATP")
