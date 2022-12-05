1136d7ffd0d58e0ba6f9ab18a54e75edd1543cc4
mocarl <50395597+mocarl@users.noreply.github.com>
2022-12-05 21:10:10 +0100

    Update quickPBSA_trace_step_analysis.py

--- PBSA/quickPBSA_trace_step_analysis.py
+++ PBSA/quickPBSA_trace_step_analysis.py
@@ -1,19 +1,18 @@
-import quickpbsa as pbsa
-import matplotlib.pyplot as plt
 import numpy as np
+import json
+import matplotlib.pyplot as plt
 import pandas as pd
-import tifffile as tifffile
-import multiprocessing as mp
+import quickpbsa as pbsa
+
 
 tracefile = '/Volumes/Carl_Ext_2/EXPERIMENTS/20221205/Top2a_25nM_MYC_100nM_BSAslides_PBSA/Experiment-343_difference.csv'
 pd.read_csv(tracefile, header=1).loc[:71, :]
 
 KVthreshold = 100
-KV_maxiter = 100
-pardict = {'norm': 1000,
-           'crop': False}
+KV_maxiter = 200
 
-pbsa.pbsa_file(tracefile, KVthreshold, KV_maxiter, preliminary_optional=pardict, num_cores=4)
+
+
 
 kvout, kv_outfile, kv_jsonfile = pbsa.steps_preliminary.kv_file(tracefile, KVthreshold, KV_maxiter)
 
